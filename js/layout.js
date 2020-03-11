2c2129cef873b50b7fe34573faf1883ed80d67ca
Eshi <eshijalote@gmail.com>
2020-03-11 11:03:15 -0500

    initial

---
+++ js/layout.js
@@ -0,0 +1,114 @@
+var Layout = function () {
+
+
+    var handleHeaderOnScroll = function () {
+        if ($(window).scrollTop() > 60) {
+            $("body").addClass("page-on-scroll");
+        } else {
+            $("body").removeClass("page-on-scroll");
+        }
+    }
+
+
+    var handleOnePageHeader = function () {
+        // jQuery to collapse the navbar on scroll
+        if ($('.navbar').offset().top > 150) {
+            $('.navbar-fixed-top').addClass('top-nav-collapse');
+        }
+        $(window).scroll(function () {
+            if ($('.navbar').offset().top > 150) {
+                $('.navbar-fixed-top').addClass('top-nav-collapse');
+            } else {
+                $('.navbar-fixed-top').removeClass('top-nav-collapse');
+            }
+        });
+
+        var $offset = 0;
+        $offset = $(".navbar-fixed-top").height() - 20;
+
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
+        $('.js_nav-item a').bind('click', function (event) {
+            var $position = $($(this).attr('href')).offset().top;
+            $('html, body').stop().animate({
+                scrollTop: $position - $offset
+            }, 600);
+            event.preventDefault();
+        });
+
+        var $scrollspy = $('body').scrollspy({target: '.navbar-fixed-top', offset: $offset + 2});
+
+
+        $(window).scroll(function () {
+            $('.navbar-collapse.in').collapse('hide');
+        });
+    }
+
+
+    var handleHeight = function () {
+        $('[data-auto-height]').each(function () {
+            var parent = $(this);
+            var items = $('[data-height]', parent);
+            var height = 0;
+            var mode = parent.attr('data-mode');
+            var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
+
+            items.each(function () {
+                if ($(this).attr('data-height') == "height") {
+                    $(this).css('height', '');
+                } else {
+                    $(this).css('min-height', '');
+                }
+
+                var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
+                if (height_ > height) {
+                    height = height_;
+                }
+            });
+
+            height = height + offset;
+
+            items.each(function () {
+                if ($(this).attr('data-height') == "height") {
+                    $(this).css('height', height);
+                } else {
+                    $(this).css('min-height', height);
+                }
+            });
+
+            if (parent.attr('data-related')) {
+                $(parent.attr('data-related')).css('height', parent.height());
+            }
+        });
+    }
+
+    return {
+        init: function () {
+
+            handleHeaderOnScroll();
+            handleOnePageHeader();
+            handleHeight();
+            $(window).scroll(function () {
+                handleHeaderOnScroll();
+            });
+        },
+
+        // To get the correct viewport width based on  http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
+        getViewPort: function () {
+            var e = window,
+                a = 'inner';
+            if (!('innerWidth' in window)) {
+                a = 'client';
+                e = document.documentElement || document.body;
+            }
+
+            return {
+                width: e[a + 'Width'],
+                height: e[a + 'Height']
+            };
+        },
+    };
+}();
+
+$(document).ready(function () {
+    Layout.init();
+});
